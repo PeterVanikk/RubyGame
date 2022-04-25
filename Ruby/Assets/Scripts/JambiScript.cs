1ab4969eb23ff258ac566e1984e18fbb3060dc39
PeterVanikk <90847947+PeterVanikk@users.noreply.github.com>
2022-04-25 09:52:17 -0400

    started audio and made diff canvas
    monday class

--- Ruby/Assets/Scripts/JambiScript.cs
+++ Ruby/Assets/Scripts/JambiScript.cs
@@ -5,30 +5,41 @@ using UnityEngine;
 public class JambiScript : MonoBehaviour
 {
     public GameObject dialogbox;
+    public GameObject dialogboxThanks;
     float timerDisplay;
     public float displayTime = 4.0f;
     void Start()
     {
+        dialogboxThanks.SetActive(false);
         dialogbox.SetActive(false);
         timerDisplay = -1.0f;
+        EnemyController controller = GetComponent<EnemyController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timerDisplay >=0)
+        if (timerDisplay >= 0)
         {
             timerDisplay -= Time.deltaTime;
-            if(timerDisplay < 0)
+            if (timerDisplay < 0)
             {
                 dialogbox.SetActive(false);
+                dialogboxThanks.SetActive(false);
             }
         }
     }
     public void DisplayDialog()
     {
         timerDisplay = displayTime;
-        dialogbox.SetActive(true);
+        if (GameObject.Find("enemy").GetComponent<EnemyController>().broken)
+        {
+            dialogbox.SetActive(true);
+        }
+        else
+        {
+            dialogboxThanks.SetActive(true);
+        }
     }
 }
 
