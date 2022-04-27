1494449aa3681b0d36fc9dad941d02d4559a46d3
PeterVanikk <90847947+PeterVanikk@users.noreply.github.com>
2022-04-27 09:50:32 -0400

    ;(
    wednesday class

--- 2D Platformer Game/Assets/Scripts/MainCharController.cs
+++ 2D Platformer Game/Assets/Scripts/MainCharController.cs
@@ -8,17 +8,30 @@ public class MainCharController : MonoBehaviour
 
     Rigidbody2D rigidbody2d;
     float horizontal;
-    
+
+    private SpriteRenderer spriteRenderer;
+
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rigidbody2d = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
+
+        Vector2 move = new Vector2(horizontal, 0);
+
+        if (!Mathf.Approximately(move.x, 0.0f))
+        {
+            spriteRenderer.flipX = true;
+        }
     }
     
     void FixedUpdate()
