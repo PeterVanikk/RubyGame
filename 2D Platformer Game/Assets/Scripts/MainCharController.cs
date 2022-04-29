f6b949e147ee249204d8a787a458672e73c648c5
PeterVanikk <90847947+PeterVanikk@users.noreply.github.com>
2022-04-28 23:39:39 -0400

    lotsmovement
    thursday all day aft school :O

--- 2D Platformer Game/Assets/Scripts/MainCharController.cs
+++ 2D Platformer Game/Assets/Scripts/MainCharController.cs
@@ -5,27 +5,39 @@ using UnityEngine;
 public class MainCharController : MonoBehaviour
 {
     public float speed = 3.0f;
-
-    bool isInvincible;
-    float invincibleTimer;
-
     Rigidbody2D rigidbody2d;
+
+    public float jumpForce;
+    public float dashForce;
+    public Transform feet;
 
     float horizontal;
     float vertical;
-    Animator animator;
+    public LayerMask groundLayers;
+    //dash
+    public float dashTime = 0.3f;
+    public float currentDashTime;
+    public float dashCoolDown = 3f;
+    public float currentDashCooldown;
+    public bool dashAllowed;
+    public bool dashActive = false;
+    public bool dash = false;
 
-    // Start is called before the first frame update
+    Animator animator;
     void Start()
     {
+        animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
+        horizontal = Input.GetAxisRaw("Horizontal");
 
         Vector2 move = new Vector2(horizontal, vertical);
 
@@ -39,19 +51,102 @@ public class MainCharController : MonoBehaviour
         }
         if (!Mathf.Approximately(move.x, 0.0f))
         {
-            animator.SetBool("IsRunning", true);
+            animator.SetBool("isRunning", true);
         }
-        if(Mathf.Approximately(move.x, 0.0f))
+        if (Mathf.Approximately(move.x, 0.0f))
         {
-            animator.SetBool("IsRunning", false);
+            animator.SetBool("isRunning", false);
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+            //animator.enabled = false;
+        }
+        if (!Input.GetKey(KeyCode.J))
+        {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+               /* if (dashAllowed)
+                {
+                    GameObject smokeObject = Instantiate(smokePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+                }*/
+                if (dash == true)
+                {
+                    return;
+                }
+                dash = true;
+                currentDashCooldown = dashCoolDown;
+                currentDashTime = dashTime;
+                dashAllowed = false;
+            }
+            if (dash == true)
+            {
+                if (currentDashTime > 0)
+                {
+                    Dash();
+                    currentDashTime -= Time.deltaTime;
+                }
+                currentDashCooldown -= Time.deltaTime;
+                currentDashTime -= Time.deltaTime;
+            }
+            if (currentDashCooldown < 0)
+            {
+                dash = false;
+                dashAllowed = true;
+            }
+        
+        if (currentDashTime > 0)
+        {
+            dashActive = true;
+        }
+        else
+        {
+            dashActive = false;
         }
     }
-
     void FixedUpdate()
     {
-        Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-
-        rigidbody2d.MovePosition(position);
+        Vector2 movement = new Vector2(horizontal * speed, rigidbody2d.velocity.y);
+        rigidbody2d.velocity = movement;
+    }
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rigidbody2d.velocity.x, jumpForce);
+        rigidbody2d.velocity = movement;
+        animator.SetTrigger("Jump");
+    }
+    public bool IsGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+        if(groundCheck != null)
+        {
+            return true;
+        }
+        return false;
+    }
+    void Dash()
+    {
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+        Vector2 movement = new Vector2(dashForce, 0.0f);
+        if(transform.localScale.x > 0)
+        {
+            if(horizontal==0)
+            {
+                transform.Translate(movement);
+            }
+            transform.Translate(movement*1.5f);
+        }
+        if(transform.localScale.x < 0)
+        {
+            if(horizontal==0)
+            {
+                transform.Translate(-movement);
+            }
+            transform.Translate(-movement*1.5f);
+        }
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        animator.SetTrigger("Dash");
     }
 }
