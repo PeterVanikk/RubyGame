c148e2cb382409e870bcd026f80fea2be49a1a9c
PeterVanikk <90847947+PeterVanikk@users.noreply.github.com>
2022-05-17 00:32:11 -0400

    enemy movement done
    monday aft school

--- 2D Platformer Game/Assets/Scripts/SkeletonIdle.cs
+++ 2D Platformer Game/Assets/Scripts/SkeletonIdle.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 
 public class SkeletonIdle : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+
     public float speed;
     public float distance;
 
-    public bool movingRight = true;
-
+    public bool noplayer;
     public Transform groundDetection;
+
+    Vector2 lookDirection = new Vector2(1, 0);
+    void Start()
+    {
+        lookDirection.Set(1f, 0f);
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        GameObject player = GameObject.Find("MainCharacter");
+    }
     void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        lookDirection.Set(transform.localScale.x, 0.0f);
+        if(noplayer==true)
+        {
+            transform.Translate(lookDirection * speed * Time.deltaTime);
+        }
 
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
         if(groundInfo.collider == false)
         {
-            if(movingRight == true)
-            {
-                transform.localScale = new Vector2(-1f, 1f);
-                transform.Translate(Vector2.left * speed * Time.deltaTime);
-                movingRight = false;
-            }
-            if(movingRight == false)
-            {
-                transform.localScale = new Vector2(1f, 1f);
-                transform.Translate(Vector2.right * speed * Time.deltaTime);
-                movingRight = true;
-            }
+           if(transform.localScale.x == 1f)
+           {
+               transform.localScale = new Vector2(-1f, 1f);
+           }
+           else
+           {
+               transform.localScale = new Vector2(1f, 1f);
+           }
+        }
+        Debug.Log(lookDirection);
+        RaycastHit2D playerchk = Physics2D.Raycast(rigidbody2d.position, lookDirection, 6.5f, LayerMask.GetMask("Player"));
+        RaycastHit2D playerbehindchk = Physics2D.Raycast(rigidbody2d.position, -lookDirection, 3.5f, LayerMask.GetMask("Player"));
+        if(playerchk.collider != null)
+        {
+           noplayer = false;
+        }
+        if(playerbehindchk.collider != null)
+        {
+           noplayer = false;
+           transform.localScale = new Vector2 (-transform.localScale.x, 1f);
+        }
+        if(playerbehindchk.collider == null && playerchk.collider == null)
+        {
+            noplayer = true;
         }
     }
 }
